5b6f11debcf3a73ef690339ef906199879d99faa
alex_drg <oleksandr.deryhlazov75@gmail.com>
2018-09-07 20:25:37 +0300

    add js for modal form and interactive map

---
+++ js/popup.js
@@ -0,0 +1,33 @@
+		var link = document.querySelector(".button-appointment");
+		var popup = document.querySelector(".modal-search");
+		var fieldCheckIn = popup.querySelector("[name=date-check-in]");
+		var fieldCheckOut = popup.querySelector("[name=date-check-out]");
+		var fieldQuantityAdult = popup.querySelector("[name=quantity-adult]");
+		var fieldQuantityChildren = popup.querySelector("[name=quantity-children]");
+		var form = popup.querySelector("form");
+		
+		link.addEventListener("click", function (evt) {
+			evt.preventDefault(); 
+			popup.classList.toggle("modal-show");
+			popup.classList.remove("modal-error");
+			fieldCheckIn.focus();
+		});
+
+		form.addEventListener("submit", function(evt) {
+			if(!fieldCheckIn.value || !fieldCheckOut.value || !fieldQuantityAdult.value || !fieldQuantityChildren.value) {
+				evt.preventDefault();
+				popup.classList.remove("modal-error");
+				popup.offsetWidth = popup.offsetWidth;
+				popup.classList.add("modal-error");
+			}
+		});
+
+		window.addEventListener("keydown", function(evt){
+			if (evt.keyCode === 27) {
+				evt.preventDefault();
+				if (popup.classList.contains("modal-show")) {
+					popup.classList.remove("modal-show");
+					popup.classList.remove("modal-error");
+				}
+			}
+		});
